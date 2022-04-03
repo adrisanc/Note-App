20dcf6f489356f30e4537780e8528c733af46b83
adrisanc <yasminadrianasanchez@gmail.com>
2022-04-03 13:56:14 -0500

    [ADD] heroku settings

--- src/models/database.js
+++ src/models/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose'); // connect app to a DB
 
 
-mongoose.connect("mongodb://localhost:27017/app-notes-db");
+mongoose.connect("mongodb+srv://maku:maku261780.@cluster0.ocilb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,'DB connection error!'));
 db.once('open', function(){
