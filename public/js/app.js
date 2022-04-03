8f05e8cb3ddf3fe45eedc7a18e178ffb18a07eea
adrisanc <yasminadrianasanchez@gmail.com>
2022-04-03 12:41:18 -0500

    [Add] estilos css y js frontend

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
-function changeColor() {
-    document.getElementById("saveBtn").style.backgroundColor = "red";
-}
+let filterDesktop = document.createElement('div');
+
+filterDesktop.className ="filter-desktop";
+let textFilters = `<div class=" filter-desktop d-flex col-md-2 justify-content-center dark-color-theme m-2">
+<span class="filter"><a>All</a></span>
+<span ><a>Active</a></span>
+<span ><a>Completed</a></span>
+</div>`
+
+
+
+let clearTask = document.getElementById('clear-task')
+let filterMobile= document.getElementById('filter-mobile')
+filterDesktop.innerHTML = textFilters
+filterMobile.innerHTML =textFilters
+clearTask.parentNode.insertBefore(filterDesktop,clearTask)
--- src/models/Task.js
+++ src/models/Task.js
@@ -2,7 +2,7 @@ const  mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema(
     {
-      title: { type: String, required: true, trim: true, unique: true },
+      title: { type: String, required: true, trim: true},
       
       done: {
         type: Boolean,
@@ -14,5 +14,8 @@ const TaskSchema = new mongoose.Schema(
       versionKey: false,
     }
   );
+  TaskSchema.path('title').validate( () => {
+    return true
+  },"Task name already exists")
   const Task = mongoose.model('Task', TaskSchema) 
 module.exports = Task;
